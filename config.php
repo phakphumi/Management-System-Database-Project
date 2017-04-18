9d2bd977d9947e03630c4e189eaa9b502e678ce8
Joe_Pakpoom <pakpoom.thawee@gmail.com>
2017-04-18 21:36:56 +0700

    template for get api

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "123456789";
+$dbname = "db_mgt_sys_design";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+
+?>
